da2ac8258c7613d71e6518e53ed429e0bf22487c
fbtty <115198968+fbtty@users.noreply.github.com>
2022-10-15 10:26:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,54 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(number)
-    number += -1
-})
 let number = 0
-number = 9
+input.onButtonPressed(Button.A, function () {
+    number = 9
+    while (number >= 0) {
+        basic.showNumber(number)
+        number += -1
+    }
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
